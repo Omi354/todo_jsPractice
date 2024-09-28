90f7e0472630bc5974e521a4f92b2d75c0ed8c64
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-09-29 01:17:08 +0900

    登録ボタンを押すと、インプットタグの入力内容をオブジェクト化して配列に挿入する機能を実装しました

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+let todoList = [];
+
 document.addEventListener("DOMContentLoaded", () => {
-  window.alert("it's working");
+  const registerBtnElem = document.getElementById("register");
+  registerBtnElem.addEventListener("click", () => {
+    // 入力されたtodoの値を取得
+    const newTodoName = document.getElementById("new-todo-name");
+    const newPerson = document.getElementById("new-person");
+    const newDeadline = document.getElementById("new-deadline");
+
+    const newTodoObject = {
+      todoName: newTodoName.value,
+      person: newPerson.value,
+      deadline: newDeadline.value,
+    };
+
+    todoList.push(newTodoObject);
+  });
+
+  const indexTodoElem = document.getElementById("index-todo");
+  console.log(indexTodoElem);
 });
