2b9e6719b21923928af643c697ed8d425a098aab
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-09-29 02:20:24 +0900

    追加したTODOを表示できるように実装しました

--- js/index.js
+++ js/index.js
@@ -1,22 +1,52 @@
 let todoList = [];
 
+// 入力されたtodoの値を取得しオブジェクト化し配列に挿入
+const registerNewTodo = () => {
+  const newTodoName = document.getElementById("new-todo-name");
+  const newPerson = document.getElementById("new-person");
+  const newDeadline = document.getElementById("new-deadline");
+  todoList.push({
+    todoName: newTodoName.value,
+    person: newPerson.value,
+    deadline: newDeadline.value,
+  });
+};
+
+// Todoリストを一旦すべて削除
+const removeTodoListElem = () => {
+  const tbodyElem = document.getElementById("todo-list");
+  tbodyElem.replaceChildren();
+};
+
+// todoList配列の中身をすべて表示
+const appendTodoListElem = () => {
+  todoList.forEach((todo) => {
+    const todoNameTdElem = document.createElement("td");
+    todoNameTdElem.textContent = todo.todoName;
+
+    const personTdElem = document.createElement("td");
+    personTdElem.textContent = todo.person;
+
+    const deadlineTdElem = document.createElement("td");
+    deadlineTdElem.textContent = todo.deadline;
+
+    const trElem = document.createElement("tr");
+    trElem.appendChild(todoNameTdElem);
+    trElem.appendChild(personTdElem);
+    trElem.appendChild(deadlineTdElem);
+
+    const tbodyElem = document.getElementById("todo-list");
+    tbodyElem.appendChild(trElem);
+  });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   const registerBtnElem = document.getElementById("register");
   registerBtnElem.addEventListener("click", () => {
-    // 入力されたtodoの値を取得
-    const newTodoName = document.getElementById("new-todo-name");
-    const newPerson = document.getElementById("new-person");
-    const newDeadline = document.getElementById("new-deadline");
+    registerNewTodo();
 
-    const newTodoObject = {
-      todoName: newTodoName.value,
-      person: newPerson.value,
-      deadline: newDeadline.value,
-    };
+    removeTodoListElem();
 
-    todoList.push(newTodoObject);
+    appendTodoListElem();
   });
-
-  const indexTodoElem = document.getElementById("index-todo");
-  console.log(indexTodoElem);
 });
